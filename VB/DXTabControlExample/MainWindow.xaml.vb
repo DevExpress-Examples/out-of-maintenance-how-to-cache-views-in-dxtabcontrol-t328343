d45fe6b0804be6ca1c3dcef13941c8b85c526fca
DevExpressExampleBot <fakeEmail>
2022-12-16 19:50:03 +0400

    Source auto update [skip ci]

--- VB/DXTabControlExample/MainWindow.xaml.vb
+++ VB/DXTabControlExample/MainWindow.xaml.vb
@@ -1,35 +1,22 @@
-﻿Imports DevExpress.Xpf.Bars
 Imports DevExpress.Xpf.Core
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace DXTabControlExample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits DXWindow
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits DXWindow
 
-		Private Sub OnShowTestWindowButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim window As New TestWindow(CType(cacheModeComboBox.EditValue, TabContentCacheMode))
-			window.Show()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-	End Class
+        Private Sub OnShowTestWindowButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim window As TestWindow = New TestWindow(CType(Me.cacheModeComboBox.EditValue, TabContentCacheMode))
+            window.Show()
+        End Sub
+    End Class
 End Namespace
--- VB/DXTabControlExample/TestWindow.xaml.vb
+++ VB/DXTabControlExample/TestWindow.xaml.vb
@@ -1,38 +1,30 @@
-﻿Imports DevExpress.Xpf.Core
-Imports System
-Imports System.Collections.Generic
+Imports DevExpress.Xpf.Core
 Imports System.Collections.ObjectModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Shapes
 
 Namespace DXTabControlExample
-	''' <summary>
-	''' Interaction logic for TestWindow.xaml
-	''' </summary>
-	Partial Public Class TestWindow
-		Inherits Window
 
-		Public Sub New(ByVal tabCacheMode As TabContentCacheMode)
-			InitializeComponent()
-			tabControl.TabContentCacheMode = tabCacheMode
-			Dim items As New ObservableCollection(Of Item)()
-			For i As Integer = 0 To 14
-				items.Add(New Item() With {.Name = "Item" & i})
-			Next i
-			tabControl.ItemsSource = items
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for TestWindow.xaml
+    ''' </summary>
+    Public Partial Class TestWindow
+        Inherits Window
 
-	Public Class Item
-		Public Property Name() As String
-	End Class
+        Public Sub New(ByVal tabCacheMode As TabContentCacheMode)
+            Me.InitializeComponent()
+            Me.tabControl.TabContentCacheMode = tabCacheMode
+            Dim items As ObservableCollection(Of Item) = New ObservableCollection(Of Item)()
+            For i As Integer = 0 To 15 - 1
+                items.Add(New Item() With {.Name = "Item" & i})
+            Next
+
+            Me.tabControl.ItemsSource = items
+        End Sub
+    End Class
+
+    Public Class Item
+
+        Public Property Name As String
+    End Class
 End Namespace
